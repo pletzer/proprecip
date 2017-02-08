761d7e390fc6d02af542ea00bbe2a61a927249d7
Alexander Pletzer <alexander@gokliya.net>
2017-02-08 17:00:29 +1300

    nowa reading the lats and lons

--- py/prob_precip.py
+++ py/prob_precip.py
@@ -1,8 +1,10 @@
 import iris
 import pnumpy
+import numpy
 from mpi4py import MPI
 import argparse
 import sys
+import netCDF4
 
 iris.FUTURE.netcdf_promote = True
 
@@ -12,6 +14,8 @@ parser.add_argument('--file', type=str, dest='file',
                     help='File containing total_precip data')
 parser.add_argument('--threshold', type=float, dest='threshold', default=8.33333333333333e-07,
                     help='Min amount of rain',)
+parser.add_argument('--niter', type=int, dest='niter', default=1, 
+                    help='Number of iterations')
 parser.add_argument('--plot', dest='plot', action='store_true', help='Plot')
 
 args = parser.parse_args()
@@ -21,6 +25,10 @@ pe = MPI.COMM_WORLD.Get_rank()
 
 # read the data
 cube = iris.load(args.file, iris.Constraint(cube_func = lambda c: c.var_name == 'total_precip'))[0]
+f = netCDF4.Dataset(args.file)
+lats = f.variables['latitude'][:]
+lons = f.variables['longitude'][:]
+f.close()
 
 # domain decomp
 size = cube.data.shape[1:] # first dimension is time
@@ -47,23 +55,32 @@ op.addStencilBranch(( 0,  0), 1.0/9.0)
 
 # apply 
 print('applying stencil..')
-out = op.apply(cube.data[0,...])
+precip = numpy.array(cube.data[0,...] > args.threshold, numpy.float64)
+for i in range(10):
+    out = op.apply(precip)
+    precip = out
 print('done.')
 
 # plot
 if args.plot:
     from matplotlib import pylab
+
     coords = cube.coords()
-    #print(coords)
-    #yy = coords[0].points
-    #xx = coords[1].points
-    #print(cube.data.shape)
-    #print(yy.shape)
+
+    print(lats.shape)
+    print(lons.shape)
+    print(out.shape)
+
     pylab.figure(1)
     data = cube.data[0,...]
-    print(data.shape)
-    pylab.pcolor(data)
+    print(data.shape)   
+    p1 = pylab.pcolor(lons, lats, data)
+    pylab.title('total_precip kg/m^2')
+    pylab.colorbar(p1)
+
     pylab.figure(2)
-    pylab.pcolor(out)
+    p2 = pylab.pcolor(lons, lats, out, vmin=0, vmax=1)
+    pylab.colorbar(p2)
+    pylab.title('probability')
     pylab.show()
 
