db3094451268c4e75126055a841a42b5d23759db
Alex Pletzer <alexander@gokliya.net>
2017-02-08 21:33:19 +1300

    more paralellism

--- py/prob_precip.py
+++ py/prob_precip.py
@@ -1,4 +1,3 @@
-import iris
 import pnumpy
 import numpy
 from mpi4py import MPI
@@ -7,8 +6,6 @@ import sys
 import netCDF4
 from matplotlib import pylab
 import time
-
-iris.FUTURE.netcdf_promote = True
 
 parser = argparse.ArgumentParser(description='Compute the probability of precipitation')
 parser.add_argument('--file', type=str, dest='file', 
@@ -22,30 +19,44 @@ parser.add_argument('--plot', dest='plot', action='store_true', help='Plot')
 
 args = parser.parse_args()
 
+def plotProb(lats, lons, data, title):
+    from mpl_toolkits.basemap import Basemap
+
+    # plot the data
+    f = pylab.figure()
+    p = pylab.pcolor(lons, lats, data, vmin=0, vmax=1)
+    pylab.colorbar(p)
+    # add the basemap
+    mp = Basemap(llcrnrlon=160, urcrnrlon=185, llcrnrlat=-49, urcrnrlat=-30, resolution='h')
+    mp.drawcoastlines(color='w')
+    f.savefig('prob_precip_pe{}.png'.format(pe))
+
 nprocs = MPI.COMM_WORLD.Get_size()
 pe = MPI.COMM_WORLD.Get_rank()
 
-# read the data
-cube = iris.load(args.file, iris.Constraint(cube_func = lambda c: c.var_name == 'total_precip'))[0]
 f = netCDF4.Dataset(args.file)
-lats = f.variables['latitude'][:]
-lons = f.variables['longitude'][:]
-f.close()
+size = f.variables['total_precip'].shape[1:]
+
 
 # domain decomp
-size = cube.data.shape[1:] # first dimension is time
 dc = pnumpy.CubeDecomp(nprocs, size)
 if not dc.getDecomp():
     print('ERROR: cannot find a valid decomposition for {} procs and size {}'.format(nprocs, size))
     sys.exit(1)
 
-ndims = dc.getNumDims()
 # local start/end grid indices
 slab = dc.getSlab(pe)
 ibeg, iend = slab[0].start, slab[0].stop
 jbeg, jend = slab[1].start, slab[1].stop
 
-# average the data 
+# read the local data
+total_precip = f.variables['total_precip'][0, ibeg:iend, jbeg:jend] # first time step
+lats = f.variables['latitude'][ibeg:iend, jbeg:jend]
+lons = f.variables['longitude'][ibeg:iend, jbeg:jend]
+
+f.close()
+
+# create averager
 op = pnumpy.StencilOperator(dc, periodic=(False, False))
 op.addStencilBranch((+1,  0), 1.0/4.0)
 op.addStencilBranch(( 0, +1), 1.0/4.0)
@@ -61,41 +72,27 @@ if args.plot:
     f.savefig('precip.png')
 """
 
-print(lats.min())
-print(lats.max())
-print(lons.min())
-print(lons.max())
 
-lats = lats[ibeg:iend, jbeg:jend]
-lons = lons[ibeg:iend, jbeg:jend]
 
-def plotProb(data, title, index):
-    from mpl_toolkits.basemap import Basemap
-
-    # plot the data
-    f = pylab.figure()
-    p = pylab.pcolor(lons, lats, data, vmin=0, vmax=1)
-    pylab.colorbar(p)
-    # add the basemap
-    mp = Basemap(llcrnrlon=160, urcrnrlon=185, llcrnrlat=-49, urcrnrlat=-30, resolution='h')
-    mp.drawcoastlines(color='w')
-    f.savefig('prob_precip_pe{}_index{}.png'.format(pe, index))
-
-# apply 
-if pe == 0: print('applying stencil..')
-precip = numpy.array(cube.data[0, ibeg:iend, jbeg:jend] > args.threshold, numpy.float64)
+# apply
+totaltime = 0
+precip = numpy.array(total_precip > args.threshold, numpy.float64)
 for i in range(args.niter):
     t0 = time.time()
     out = op.apply(precip)
     precip[...] = out
-    # compute global checksums
-    checksum = out.sum()
-    checksum0 = numpy.sum(MPI.COMM_WORLD.gather(checksum, 0))
     t1 = time.time()
-    if pe == 0: 
-        print('i = {} checksum: {} time: {}'.format(i, checksum0, t1 - t0))
-    # plot data
-    if args.plot: 
-        plotProb(out, 'prob precip i = {}'.format(i), i)
-if pe == 0: print('done.')
+    totaltime += t1 - t0
+
+# compute global checksums and time
+totaltime /= float(nprocs) # average
+totaltime0 = numpy.sum(MPI.COMM_WORLD.gather(totaltime, 0))
+checksum = out.sum()
+checksum0 = numpy.sum(MPI.COMM_WORLD.gather(checksum, 0))
+if pe == 0: 
+    print('i = {} checksum: {} time: {}'.format(i, checksum0, totaltime0))
+
+# plot data
+if args.plot: 
+    plotProb(out, 'prob precip')
 
